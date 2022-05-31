3b6ed42b4a3acca6e4e756ca8e5d9ea4e4016d16
Toh-Ma <92999833+Toh-Ma@users.noreply.github.com>
2022-05-31 10:28:00 +0200

    Rest Controller

--- chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/controller/MessageRestController.java
@@ -24,23 +24,21 @@ public class MessageRestController {
 		RestTemplate restTemplate = new RestTemplate();
 		ApiResponseUpdateTelegram update = restTemplate.getForObject("https://api.telegram.org/bot5348334660:AAGeyqcd-6MqfuhFhElqoZYsjKCHeboiQZM/getUpdates",
 				ApiResponseUpdateTelegram.class);
-
-
 		return ResponseEntity.ok().body(update);
 	}
 
 	@PostMapping("/sendMessage")
 	public static ResponseEntity<ApiResponseTelegram> sendMessage(@RequestParam("text") String text, @RequestParam("chat_id") String chat_id) {
-
 		RestTemplate restTemplate = new RestTemplate();
 		ApiResponseTelegram sendMessage = restTemplate.getForObject("https://api.telegram.org/bot5348334660:AAGeyqcd-6MqfuhFhElqoZYsjKCHeboiQZM/sendMessage?text={text}"+"&chat_id={chat_id}",
 				ApiResponseTelegram.class,text,chat_id);
-
-
 		return ResponseEntity.ok().body(sendMessage);
 	}
 
-
-
-
+	@PostMapping("/deleteUpdate")
+	public static ResponseEntity<ApiResponseTelegram> deleteUpdate(@RequestParam("offset") int offset) {
+		RestTemplate restTemplate = new RestTemplate();
+		ApiResponseTelegram deleteUpdate = restTemplate.getForObject("https://api.telegram.org/bot5348334660:AAGeyqcd-6MqfuhFhElqoZYsjKCHeboiQZM/getupdates?offset={offset}",ApiResponseTelegram.class,offset);
+		return ResponseEntity.ok().body(deleteUpdate);
+	}
 }
